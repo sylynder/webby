c11df6483e1f0a2f4ff499d975a3c0cec4fcabe2
otengkwame <developerkwame@gmail.com>
2020-08-09 23:56:09 +0000

    added two method aliases for manipulationg urls

--- app/helpers/ci_core_helper.php
+++ app/helpers/ci_core_helper.php
@@ -5,7 +5,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 if ( ! function_exists('ci')) 
 {
     /**
-     *  CodeIgniter Instance method
+     *  CodeIgniter Instance function
      */
 
     function ci()
@@ -13,3 +13,43 @@ if ( ! function_exists('ci'))
         return $ci = &get_instance();
     }
 }
+
+/* ------------------------------- Uri Functions ---------------------------------*/
+
+if ( ! function_exists('app_url')) 
+{
+    /**
+     * alias of base_url.
+     *
+     * @param string $uri
+     * @param bool $protocol
+     * @return void
+     */
+    function app_url($uri = '', $protocol = NULL)
+    {   
+        return base_url($uri, $protocol);
+    }
+}
+
+if ( ! function_exists('url')) 
+{
+    /**
+     * alias of site_url
+     *
+     * @param string $uri
+     * @param bool $protocol
+     * @return void
+     */
+    function url($uri = '', $protocol = NULL)
+    {
+        
+        if ($uri === 'void') {
+            return void_url();
+        }
+
+        return site_url($uri, $protocol);
+    }
+}
+
+/* ------------------------------- Uri Functions ---------------------------------*/
+
