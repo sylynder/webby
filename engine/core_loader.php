e98259bdd11a7fabab9b75ee6b27f822ea80e174
otengkwame <developerkwame@gmail.com>
2020-08-27 00:59:10 +0000

    added core_loader file

---
+++ engine/core_loader.php
@@ -0,0 +1,42 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ *  Core helpers found @ engine/Core/helpers/
+ *  
+ */
+$core_helpers = [
+
+    // 'addon',
+    // 'aauth',
+    // 'asset',
+    // 'ci_core',
+    // 'db',
+    // 'developer',
+    // 'format',
+    // 'sylynder',
+    // 'template',
+];
+
+/**
+ * Core Libraries
+*/
+$core_libraries = [
+];
+
+/**
+ * Core Models
+*/
+$core_models = [
+
+];
+
+/**
+ * Core Packages
+ * Expects strings with comma seperated not array
+ * eg. ENGINEPATH . 'third_party' , ENGINEPATH . 'location'
+ *
+*/
+$core_packages = '';
+ 
