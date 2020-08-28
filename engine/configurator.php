5f289ed5ccc0ac22d785d12e2fe853de9d1e7163
otengkwame <developerkwame@gmail.com>
2020-08-28 23:37:16 +0000

    added engine/configurator.php file

---
+++ engine/configurator.php
@@ -0,0 +1,40 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+*
+*  Load all configuration files here
+*
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Get all Configuration files
+|--------------------------------------------------------------------------
+| e.g $config['some_config_name_here'] = 'some_value_here';
+*/
+
+$files = glob(ROOTPATH . "config/*.php"); 
+
+$exclude = [
+    'autoload',
+	'constants',
+	'database',
+	'hooks', 
+	// 'settings',
+    // 'commands', 
+];
+
+foreach($files as $file)
+{  
+    foreach($exclude as $name) 
+    { 
+        if (stripos($file, $name) !== false) 
+        { 
+            continue 2; // break out of 2 levels of loops 
+        } 
+    } 
+
+    require_once $file;
+}
