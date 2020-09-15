96e3c4f177e44dc62354fafc46f47f746f1b42aa
otengkwame <developerkwame@gmail.com>
2020-09-15 23:59:30 +0000

    added: a plate_helper.php file it will contain all functions to help with templating

---
+++ engine/Core/helpers/plate_helper.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *  Plate Helper functions
+ *
+ *  @package		Webby
+ *	@subpackage		Helpers
+ *	@category		Helpers
+ *	@author			Kwame Oteng Appiah Nti
+ */
+
+// ------------------------------------------------------------------------
