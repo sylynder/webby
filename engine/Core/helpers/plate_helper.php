3f1e985d9312c34394bf7c13cfa2a17c6db345a1
otengkwame <developerkwame@gmail.com>
2020-09-16 23:56:13 +0000

    added: view function in  plate_helper file

--- engine/Core/helpers/plate_helper.php
+++ engine/Core/helpers/plate_helper.php
@@ -11,3 +11,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 
 // ------------------------------------------------------------------------
+
+if ( ! function_exists('view')) 
+{
+    /**
+     * Undocumented function
+     *
+     * @param string $view_path
+     * @param arrat $view_data
+     * @return void
+     */
+    function view($view_path, $view_data = null)
+    {
+        $view_path = dot($view_path);
+
+        ci()->load->view($view_path, $view_data);
+    }
+}
+
