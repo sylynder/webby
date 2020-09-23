daabc4cdf4f6d5f36ae01b69fcbc074360edd8a7
otengkwame <developerkwame@gmail.com>
2020-09-23 23:54:47 +0000

    added: mail_view function

--- engine/Core/helpers/plate_helper.php
+++ engine/Core/helpers/plate_helper.php
@@ -12,6 +12,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // ------------------------------------------------------------------------
 
+if ( ! function_exists('mail_view')) 
+{
+    /**
+     * Used for finding mail views
+     *
+     * @param string $mail_view_path where to find the mail view
+     * @param array $mail_data
+     * @return void
+     */
+    function mail_view($mail_view_path, $mail_data)
+    {
+        $layout = null;
+        $view = null;
+
+        $mail_view_path = dot($mail_view_path);
+
+        if (strstr($mail_view_path, '::')) {
+            $mail_view_path = string_to_array('::', $mail_view_path);
+        }
+
+        if (empty($mail_data)) {
+            $exception_message = "Email data cannot be empty as second paramater";
+            throw new Exception($exception_message); 
+            log_message('error', $exception_message);
+        }
+
+        if ( ! is_array($mail_data)) {
+            $exception_message = "Email data should be array";
+            throw new Exception($exception_message); 
+            log_message('error', $exception_message);
+        }
+
+        if ( ! is_array($mail_view_path)) {
+            $exception_message = "Email view malformed, make sure it has the 'double colon' symbol '::' in it";
+            throw new Exception($exception_message); 
+            log_message('error', $exception_message);
+            exit;
+        }
+
+        $layout = $mail_view_path[0];
+        $view = $mail_view_path[1];
+        $mail_data = add_associative_array($mail_data, 'content', $view);
+
+        return ci()->load->view($layout, $mail_data, true);
+    }
+}
+
 if ( ! function_exists('view')) 
 {
     /**
