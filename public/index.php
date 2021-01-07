651f6017fe41b3d601400878992c209f5cb49a24
otengkwame <developerkwame@gmail.com>
2021-01-07 23:27:36 +0000

    :ok_hand: Improve: added environment settings to index.php

--- public/index.php
+++ public/index.php
@@ -163,6 +163,76 @@ $upload_directory_line = __LINE__ - __ONE__;
 include_once( __DIR__ . '/../engine/bootstrap.php');
 
 /*
+ * Function to detect application 
+ * environment 
+ */
+function detect_environment()
+{
+    // Make sure ENVIRONMENT isn't already set by other means.
+    if (! defined('ENVIRONMENT'))
+    {
+        // running under Continuous Integration server?
+        if (getenv('CI') !== false)
+        {
+            define('ENVIRONMENT', 'testing');
+        }
+        else
+        {
+            return $_SERVER['app.env'] ?? 'production';
+        }
+    }
+}
+
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ */
+define('ENVIRONMENT', detect_environment());
+
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
+switch (ENVIRONMENT) {
+    case 'development':
+        error_reporting(-1);
+        ini_set('display_errors', 1);
+        break;
+
+    case 'testing':
+    case 'production':
+        ini_set('display_errors', 0);
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+        break;
+
+    default:
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'The application environment is not set correctly.';
+        exit(1); // EXIT_ERROR
+}
+
+/*
  * --------------------------------------------------------------------
  * DEFINE FRAMEWORK VERSION
  * --------------------------------------------------------------------
@@ -171,7 +241,7 @@ define('WEBBY_VERSION', 'v0.1.0');
 
 /*
  * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
+ * LOAD CODEIGNITER BOOTSTRAP FILE
  * --------------------------------------------------------------------
  *
  * And away we go...
