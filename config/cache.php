5fd6f2f797774cc3e0eacfa1960eb1bd6695c9fc
otengkwame <developerkwame@gmail.com>
2021-02-09 08:50:02 +0000

    :ok_hand: Improve: modify heavily config to suit webby

---
+++ config/cache.php
@@ -0,0 +1,48 @@
+<?php
+defined('COREPATH') or exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Your App Cache Array Configurations
+| -------------------------------------------------------------------------
+| This file lets you define some configurations
+| that can help with cache functionalities
+|
+ */
+
+//Please make sure you understand what you are doing before 
+//you change the cache configurations
+
+$config['cache_dir']  = CACHE_PATH;
+
+$config['cache_path'] = CACHE_PATH;
+
+$config['web_cache_path'] = CACHE_PATH . 'web' . DS;
+
+/*
+|--------------------------------------------------------------------------
+| Cache Include Query String
+|--------------------------------------------------------------------------
+|
+| Whether to take the URL query string into consideration when generating
+| output cache files. Valid options are:
+|
+|	FALSE      = Disabled
+|	TRUE       = Enabled, take all query parameters into account.
+|	             Please be aware that this may result in numerous cache
+|	             files generated for the same page over and over again.
+|	array('q') = Enabled, but only take into account the specified list
+|	             of query parameters.
+|
+*/
+$config['cache_query_string'] = FALSE;
+
+/*
+|--------------------------------------------------------------------------
+| Cache Expire Time
+|--------------------------------------------------------------------------
+|
+| Tells the time for cache file to expire
+|
+*/
+$config['cache_default_expires'] = 3600;
