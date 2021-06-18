78912f5a787f9e71432aa12bad3c743546282768
otengkwame <developerkwame@gmail.com>
2021-06-18 16:07:00 +0000

    :hammer: improved ApiController class

--- engine/Core/core/Controllers/ApiController.php
+++ engine/Core/core/Controllers/ApiController.php
@@ -2,6 +2,61 @@
 
 namespace Base\Controllers;
 
+use Base\Rest\HttpStatus;
 use Base\Rest\RestController;
+use Exception;
 
-class ApiController extends RestController {}
+class ApiController extends RestController 
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function setKey($controllerMethod, $value) 
+    {
+        if (!empty($controllerMethod) && !empty($value)) {
+            return $this->methods[$controllerMethod]['key'] = $value;
+        }
+        
+        throw new Exception("Specify controller method and key", HttpStatus::BAD_REQUEST);
+    }
+    
+    protected function setLimit($controllerMethod, $noOfTimes) 
+    {
+        if (!empty($controllerMethod) && !empty($noOfTimes)) {
+            return $this->methods[$controllerMethod]['limit'] = $noOfTimes;
+        }
+        
+        throw new Exception("Specify controller method and no of times", HttpStatus::BAD_REQUEST);
+    }
+
+    protected function setLevel($controllerMethod, $value) 
+    {
+        if (!empty($controllerMethod) && !empty($value)) {
+            return $this->methods[$controllerMethod]['level'] = $value;
+        }
+        
+        throw new Exception("Specify controller method and level", HttpStatus::BAD_REQUEST);
+    }
+
+    protected function setTime($controllerMethod, $value) 
+    {
+        if (!empty($controllerMethod) && !empty($value)) {
+            return $this->methods[$controllerMethod]['time'] = $value;
+        }
+        
+        throw new Exception("Specify controller method and level", HttpStatus::BAD_REQUEST);
+    }
+
+    protected function setLog($controllerMethod, $value) 
+    {
+        if (!empty($controllerMethod) && !empty($value)) {
+            return $this->methods[$controllerMethod]['log'] = $value;
+        }
+        
+        throw new Exception("Specify controller method and log", HttpStatus::BAD_REQUEST);
+    }
+
+}
