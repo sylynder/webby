747e7b3e4e2f18bc0c2c79d5b24852828b4064a9
otengkwame <developerkwame@gmail.com>
2021-07-10 22:42:58 +0000

    :sparkles: add Model that extends from CI_Model

---
+++ engine/Core/core/Models/Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Base\Models;
+
+class Model extends \CI_Model 
+{
+    /**
+     * Construct the CI_Model
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
+/* end of file Core/core/Models/Model.php */
