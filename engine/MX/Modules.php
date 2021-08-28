a33a9562983630bcbf20d556b639d5eae07db7aa
otengkwame <developerkwame@gmail.com>
2021-08-28 13:05:15 +0000

    :bug: fix each function deprecated

--- engine/MX/Modules.php
+++ engine/MX/Modules.php
@@ -80,7 +80,7 @@ class Modules
 	/** Load a module controller **/
 	public static function load($module) 
 	{
-		(is_array($module)) ? list($module, $params) = each($module) : $params = NULL;	
+		(is_array($module)) ? list($module, $params) = with_each($module) : $params = NULL;	
 		
 		/* get the requested controller class name */
 		$alias = strtolower(basename($module));
