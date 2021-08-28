c9196639a5ea6588d20a6888ea4434aa5e65630d
otengkwame <developerkwame@gmail.com>
2021-08-28 15:46:24 +0000

    :hammer: modify code structure

--- engine/Core/core/Controllers/WebController.php
+++ engine/Core/core/Controllers/WebController.php
@@ -4,14 +4,20 @@ namespace Base\Controllers;
 
 use Base\View\Plates;
 
-class WebController extends \Base_Controller {
+class WebController extends \Base_Controller 
+{
 
     public $plate;
 
     public function __construct()
     {
         parent::__construct();
-        $this->plate = plates();
+        $this->plate = $this->plates();
+    }
+
+    public function plates($params = [])
+    {
+        return (new Plates($params));
     }
 
 }
