d8942aebf8c9a0a0cafecdb6ea0d69f6e18468c7
Oteng Kwame <otengkwameit@gmail.com>
2021-11-15 12:33:44 +0000

    :hammer: improve and add new functions

--- engine/Core/core/Route/Route.php
+++ engine/Core/core/Route/Route.php
@@ -1,64 +1,92 @@
 <?php
 
 /**
- * Intelligent, elegant routing for CodeIgniter
+ * Intelligent, Elegant routing for Webby
  *
+ * This implementation by Jamie Rumblelow is great so I decided to 
+ * implement it and made much modification to work with Webby
+ * 
+ * @author Kwame Oteng Appiah-Nti (Developer Kwame)
+ * 
+ * Initially deprecated and not maintained by the author
  * @link http://github.com/jamierumbelow/pigeon
  * @copyright Copyright (c) 2012, Jamie Rumbelow <http://jamierumbelow.net>
  * 
- * Initially deprecated and not maintained by the author
- * 
- * This implementation by Jamie Rumblelow is great so I decided to 
- * implement it and make a little modification to work with Webby
- * 
- * @author Kwame Oteng Appiah-Nti (Developer Kwame)
  */
 
 namespace Base\Route;
 
-class Route 
+use Base\Helpers\Inflector;
+
+class Route
 {
 
 	/**
-     * Routes array
-     *
-     * @var array
-     */
-	// public $routes = [];
-
-    /**
-     * Temporary routes array
-     *
-     * @var array
-     */
-	// public $temporary_routes = [];
-
-    /**
-     * Route namespace
-     *
-     * @var string
-     */
-	// public $namespace = '';
-
-
+	 * Routes array
+	 *
+	 * @var array
+	 */
 	public static $routes = [];
 
+	/**
+	 * Router array
+	 *
+	 * @var array
+	 */
 	public static $router = [];
 
-	public static $temporary_routes = [];
+	/**
+	 * Temporary routes array
+	 *
+	 * @var array
+	 */
+	public static $temporaryRoutes = [];
 
+	/**
+	 * Default Routes array
+	 *
+	 * @var array
+	 */
 	public static $defaultRoutes = [];
 
+	/**
+	 * Available Routes array
+	 *
+	 * @var array
+	 */
 	public static $availableRoutes = [];
 
+	/**
+	 * Api Routes array
+	 *
+	 * @var array
+	 */
 	public static $apiRoutes = [];
 
+	/**
+	 * Route Regex variable
+	 *
+	 * @var string
+	 */
 	public static $routeRegex = '([a-zA-Z0-9\-_]+)';
 
+	/**
+	 * Route Namespace variable
+	 *
+	 * @var string
+	 */
 	public static $namespace = '';
 
+	/**
+	 * Set Http status
+	 *
+	 * @var boolean
+	 */
 	public static $trueHttp = false;
 
+	/**
+	 * Constants for route files
+	 */
 	const WEB_ROUTE = 'web';
 	const RESTFUL_ROUTE = 'api';
 	const CONSOLE_ROUTE = 'console';
@@ -68,7 +96,7 @@ class Route
 	 *
 	 * @var string
 	 */
-    public $uri = '';
+	public $uri = '';
 
 	/**
 	 * refer to this class
@@ -77,44 +105,68 @@ class Route
 	 */
 	protected static $self = null;
 
-    public function __construct($namespace = false)
+	/**
+	 * Constructor function
+	 *
+	 * @param boolean $namespace
+	 */
+	public function __construct($namespace = false)
 	{
 		if ($namespace) {
 			static::$namespace = $namespace;
 		}
 	}
 
-	//Utility functions
-	public static function toSlash($str)
+	// --------------------------- Utility functions ----------------------------------
+
+	/**
+	 * Replace dot to slash
+	 *
+	 * @param string $string
+	 * @return string
+	 */
+	public static function toSlash($string): string
 	{
-		$str = is_array($str) ? $str : dot2slash($str);
+		$string = is_array($string) ? $string : dot2slash($string);
 
-        if (strstr($str, '/')) {
-            $str = $str;
-        }
+		if (strstr($string, '.')) {
+			$string = str_replace('.', '/', $string);
+		}
 
-        if (strstr($str, '.')) {
-            $str = str_replace('.', '/', $str);
-        }
-
-        return $str;
+		return $string;
 	}
 
+	/**
+	 * Get CodeIgniter Router Instance
+	 *
+	 * @return object
+	 */
 	public static function getRouter()
 	{
 		return static::$router = ci()->router;
 	}
 
-    private function getUri() 
+	/**
+	 * Get Uri
+	 *
+	 * @return self
+	 */
+	private function getUri()
 	{
 		return $this->uri;
 	}
-    
-    public function setRoute($uri = null)
+
+	/**
+	 * Set route
+	 *
+	 * @param mixed $uri
+	 * @return self
+	 */
+	public function setRoute($uri = null)
 	{
-		if ( ! empty($uri)) {
+		if (!empty($uri)) {
 			$this->uri = ci()->config->site_url($uri);
-		} 
+		}
 
 		if ($uri === null) {
 			$this->uri = ci()->config->site_url('');
@@ -123,14 +175,14 @@ class Route
 		return $this;
 	}
 
-    public function to($uri = '') 
+	public function to($uri = '')
 	{
 		$this->uri = ci()->config->site_url($uri);
 
 		if (empty($this->uri) || is_null($this->uri)) {
 			return $this->uri = '';
 		}
-		
+
 		return $this;
 	}
 
@@ -146,20 +198,19 @@ class Route
 			return $this;
 		}
 
-		if ( ! empty($uri)) {
+		if (!empty($uri)) {
 			$uri = site_url($uri);
 		}
 
 		$referer = $uri;
-		
-		return redirect($referer);
 
+		return redirect($referer);
 	}
 
 	// public function setReferrer($value)
 	// {
 	// 	$_SESSION['_webby_previous_url'] = $value;
-		
+
 	// 	$this->refferer = $_SESSION['_webby_previous_url'];
 
 	// 	if () {
@@ -168,36 +219,51 @@ class Route
 
 	// }
 
-	public function redirect() 
+	/**
+	 * Redirect routes
+	 *
+	 * @return self
+	 */
+	public function redirect()
 	{
 
-		if ( ! empty($this->getUri())) {
+		if (!empty($this->getUri())) {
 			redirect($this->getUri());
 		}
 
 		return $this;
 	}
 
-	public function with($key, $value=null) 
+	public function with($key, $value = null)
 	{
 
 		ci('load')->library('session');
 
 		if (is_array($key)) {
-            ci()->session->set_flashdata($key);
-        }
+			ci()->session->set_flashdata($key);
+		}
 
-        if ( ! is_null($value) && is_string($key)) {
-            ci()->session->set_flashdata($key, $value);
-        }
+		if (!is_null($value) && is_string($key)) {
+			ci()->session->set_flashdata($key, $value);
+		}
 
-		if ( ! empty($this->getUri())) {
+		if (!empty($this->getUri())) {
 			return $this->redirect();
 		}
 	}
 
+	public function withSuccess($message)
+	{
+		return $this->with('success_message', $message);
+	}
+
+	public function withError($message)
+	{
+		return $this->with('error_message', $message);
+	}
+
 	// @Todo work on grabbing input
-	public function withInput()  
+	public function withInput()
 	{
 		// $input = ci()->input();
 		// $this->with($input);
@@ -206,7 +272,7 @@ class Route
 	}
 
 	// ---------------------------- Route Energized -------------------------------
-	
+
 	/**
 	 * Get all defined routes
 	 *
@@ -236,30 +302,28 @@ class Route
      * BASIC ROUTING
      * ------------------------------------------------------------ */
 	// @Todo To be implemented
-	private static function prefix($namespace, /*Closure*/ $callable = null) 
+	private static function prefix($namespace, /*Closure*/ $callable = null)
 	{
 		static::$namespace = $namespace;
 
-		$main_routes = [
-			self::CONSOLE_ROUTE, 
-			self::WEB_ROUTE, 
+		$mainRoutes = [
+			self::CONSOLE_ROUTE,
+			self::WEB_ROUTE,
 			self::RESTFUL_ROUTE
 		];
 
-		if (in_array($namespace, $main_routes)) {
-
+		if (in_array($namespace, $mainRoutes)) {
 		}
-
 	}
 
 	public static function route($from, $to, $nested = false)
 	{
 		$parameterfy = false;
-		
+
 		// Allow for array based routes and other symbol routes
-        if (!is_array($to) && strstr($to, '.')) {
-            $to = str_replace('.', '/', $to);
-        }
+		if (!is_array($to) && strstr($to, '.')) {
+			$to = str_replace('.', '/', $to);
+		}
 
 		if (is_array($to)) {
 			$to = $to[0] . '/' . strtolower($to[1]);
@@ -279,11 +343,6 @@ class Route
 		) {
 			$to = $matches[1] . '/' . $matches[2];
 			$parameterfy = true;
-		} elseif (
-			preg_match('/^([a-zA-Z\_\-0-9\/]+)~([a-zA-Z\_\-0-9\/]+)$/m', $to, $matches)
-		) {
-			$to = $matches[1] . '/' . $matches[2];
-			$parameterfy = true;
 		}
 
 		// Do we have a namespace?
@@ -297,16 +356,16 @@ class Route
 		}
 
 		// Apply our routes
-		static::$temporary_routes[$from] = $to;
+		static::$temporaryRoutes[$from] = $to;
 
 		// Do we have a nesting function?
 		if ($nested && is_callable($nested)) {
-			$nested_route = new Route($from);
-			call_user_func_array($nested, array( &$nested_route ));
-			static::$temporary_routes = array_merge(static::$temporary_routes, $nested_route->temporary_routes);
+			$nestedRoute = new Route($from);
+			call_user_func_array($nested, array(&$nestedRoute));
+			static::$temporaryRoutes = array_merge(static::$temporaryRoutes, $nestedRoute->temporaryRoutes);
 		}
 
-		static::$routes = static::$temporary_routes;
+		static::$routes = static::$temporaryRoutes;
 	}
 
 	public static function any($from, $to, $nested = false)
@@ -355,7 +414,7 @@ class Route
 	public static function head($from, $to)
 	{
 		if (
-			isset($_SERVER['REQUEST_METHOD']) && 
+			isset($_SERVER['REQUEST_METHOD']) &&
 			$_SERVER['REQUEST_METHOD'] == 'HEAD'
 		) {
 			static::route($from, $to);
@@ -365,61 +424,41 @@ class Route
 	public static function options($from, $to)
 	{
 		if (
-			isset($_SERVER['REQUEST_METHOD']) && 
+			isset($_SERVER['REQUEST_METHOD']) &&
 			$_SERVER['REQUEST_METHOD'] == 'OPTIONS'
 		) {
 			static::route($from, $to);
 		}
 	}
 
-	/* --------------------------------------------------------------
-     * RESTFUL ROUTING
-     * ------------------------------------------------------------ */
-
-	public static function resources($name, $nested = false, $html_friendly = false)
-    {
-        static::get($name, $name . '->index');
-        static::get($name . '/new', $name . '->create_new');
-        static::get($name . '/'. static::$routeRegex.'/edit', $name . '->edit');
-        static::get($name . '/'. static::$routeRegex, $name . '->show');
-        static::post($name, $name . '->create');
-
-        if ($html_friendly) {
-            static::post($name . '/'. static::$routeRegex, $name . '->update');
-            static::get($name . '/'. static::$routeRegex.'/delete', $name . '->delete');
-        } else {
-            static::put($name . '/'. static::$routeRegex, $name . '->update');
-            static::delete($name . '/'. static::$routeRegex, $name . '->delete');   
-        }
-        
-		if ($nested && is_callable($nested)) {
-			$nested_route = new Route($name . '/'. static::$routeRegex);
-			call_user_func_array($nested, array( &$nested_route ));
-			static::$temporary_routes = array_merge(
-				static::$temporary_routes, 
-				$nested_route::$temporary_routes
-			);
-		}
-	}
-
-	public static function resource($name, $nested = false)
+	public static function webResource($name, $hasController = true)
 	{
-		static::get($name, $name . '/show');
-		static::get($name . '/new', $name . '/create_new');
-		static::get($name . '/edit', $name . '/edit');
-		static::post($name, $name . '/create');
-		static::put($name, $name . '/update');
-		static::delete($name, $name . '/delete');
+		$name = str_replace('/', '.', $name);
 
-		if ($nested && is_callable($nested)) {
-			$nested_route = new Route($name);
-			call_user_func_array($nested, [&$nested_route]);
-			static::$temporary_routes = array_merge(
-				static::$temporary_routes, 
-				$nested_route::$temporary_routes
-			);
+		list($module, $controller) = explode('.', $name);
+
+		$moc = ucfirst($module) . '/' . ucfirst($controller);
+
+		if ($hasController) {
+			$controller = ucfirst(Inflector::singularize($controller)) . "Controller";
+			$moc = ucfirst($module) . '/' . $controller;
 		}
+
+		static::get($name . '.list', $moc . '/index');
+		static::get($name . '.show.(:any)', $moc . '/show/$1');
+		static::get($name . '.add', $moc . '/create');
+		static::post($name . '.save', $moc . '/store');
+		static::get($name . '.edit.(:any)', $moc . '/edit/$1');
+		static::put($name . '.update.(:any)', $moc . '/update/$1');
+		static::delete($name . '.delete.(:any)', $moc . '/delete/$1');
 	}
+
+	public static function uselinks($name, $hasController = true)
+	{
+		static::webResource($name, $hasController);
+	}
+
+	
 
 	/* --------------------------------------------------------------
      * UTILITY FUNCTIONS
@@ -436,6 +475,15 @@ class Route
 	/**
 	 * Return the routes array
 	 */
+
+	/**
+	 * Return the routes array
+	 *
+	 * Used as a helper in HMVC Routing
+	 * 
+	 * @param array $route
+	 * @return void
+	 */
 	public static function build(array $route = [])
 	{
 
@@ -450,12 +498,17 @@ class Route
 	}
 
 	/**
-	 * Extract the URL parameters from $from and copy to $to
+	 * Extract the URL parameters 
+	 * from $from and copy to $to
+	 *
+	 * @param string $from
+	 * @param string $to
+	 * @return string
 	 */
 	public static function parameterfy($from, $to)
 	{
 		if (preg_match_all('/\/\((.*?)\)/', $from, $matches)) {
-			
+
 			$params = '';
 
 			foreach ($matches[1] as $i => $match) {
@@ -469,15 +522,23 @@ class Route
 		return $to;
 	}
 
+	/**
+	 * Verify Http Method
+	 * 
+	 * And check whether it is to be 
+	 * strictly true or not
+	 *
+	 * @param string $method
+	 * @return mixed
+	 */
 	protected static function methodIs($method)
 	{
-		return (static::$trueHttp === false) 
-				? $method
-				: (isset($_SERVER['REQUEST_METHOD']) &&
-					   ($_SERVER['REQUEST_METHOD'] == $method ||
-					   		($_SERVER['REQUEST_METHOD'] == 'POST' &&
-					   		isset($_POST['_method']) &&
-					   		strtolower($_POST['_method']) == strtolower($method))));
+		return (static::$trueHttp === false)
+			? $method
+			: (isset($_SERVER['REQUEST_METHOD']) &&
+				($_SERVER['REQUEST_METHOD'] == $method ||
+					($_SERVER['REQUEST_METHOD'] == 'POST' &&
+						isset($_POST['_method']) &&
+						strtolower($_POST['_method']) == strtolower($method))));
 	}
-	
 }
