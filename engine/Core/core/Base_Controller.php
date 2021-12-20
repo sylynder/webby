9d8aceedeb908990b25e09571a3561be6bac8ad9
otengkwame <developerkwame@gmail.com>
2021-12-20 16:29:43 +0000

    :ok_hand: improve WebController
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- engine/Core/core/Base_Controller.php
+++ engine/Core/core/Base_Controller.php
@@ -10,8 +10,6 @@ class Base_Controller extends MX_Controller
     {
         parent::__construct();
 
-        $this->data['site_name'] = config_item('app_name');
-
         // Protection
         $this->output->set_header('X-Content-Type-Options: nosniff');
         $this->output->set_header('X-Frame-Options: DENY');
@@ -23,28 +21,6 @@ class Base_Controller extends MX_Controller
         // Load the CodeIgniter Database 
         // Object from here i.e $this->db
         $this->load->database();
-    }
-
-    protected function setTitle($title)
-    {
-        $this->data['page_title'] = $title;
-    }
-
-    protected function setBreadcrumb($title, $item = '', $activePage = '')
-    {
-        $this->data['breadcrumb_title'] = $title;
-        $this->data['breadcrumb_item']  = $item;
-        $this->data['active_page']      = $activePage;
-    }
-
-    protected function view($viewPath, $viewData = [], $return = false) 
-    {
-        return view($viewPath, $viewData, $return);
-    }
-
-    protected function layout($layoutPath, $viewPath = null, $viewData = null) 
-    {
-        return layout($layoutPath, $viewPath, $viewData);
     }
 
 }
--- engine/Core/core/Controllers/WebController.php
+++ engine/Core/core/Controllers/WebController.php
@@ -7,17 +7,42 @@ use Base\View\Plates;
 class WebController extends Controller
 {
 
-    public $plate;
+    protected $plate;
 
     public function __construct()
     {
         parent::__construct();
+
+        $this->data['site_name'] = config_item('app_name');
+
         $this->plate = $this->plates();
     }
 
     public function plates($params = [])
     {
         return (new Plates($params));
+    }
+
+    protected function setTitle($title)
+    {
+        $this->data['page_title'] = $title;
+    }
+
+    protected function setBreadcrumb($title, $item = '', $activePage = '')
+    {
+        $this->data['breadcrumb_title'] = $title;
+        $this->data['breadcrumb_item']  = $item;
+        $this->data['active_page']      = $activePage;
+    }
+
+    protected function view($viewPath, $viewData = [], $return = false)
+    {
+        return view($viewPath, $viewData, $return);
+    }
+
+    protected function layout($layoutPath, $viewPath = null, $viewData = null)
+    {
+        return layout($layoutPath, $viewPath, $viewData);
     }
 
 }
