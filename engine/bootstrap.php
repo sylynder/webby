8205bc701c94db00661b65636d8f77b1f1fe19b8
otengkwame <developerkwame@gmail.com>
2021-12-29 23:52:13 +0000

    :zap: update webby version to v1.0.0
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- engine/bootstrap.php
+++ engine/bootstrap.php
@@ -351,7 +351,7 @@ if (is_dir($upload_directory)) {
  * DEFINE WEBBY VERSION
  * --------------------------------------------------------------------
  */
-define('WEBBY_VERSION', 'v0.8.0');
+define('WEBBY_VERSION', 'v1.0.0');
 
 // Load environment settings from .env files
 // into $_SERVER and $_ENV
