9774eabfc164bd348908c39e1c6589947c0e25b6
otengkwame <developerkwame@gmail.com>
2022-03-01 00:22:37 +0000

    :sparkles: add use_css, use_js functions
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- engine/Core/helpers/asset_helper.php
+++ engine/Core/helpers/asset_helper.php
@@ -78,89 +78,63 @@ if ( ! function_exists('writable_path'))
 
 if ( ! function_exists('load_path')) 
 {
-	/**
+    /**
      * Load other folders from the public folder
      * @param  string $path
      * @return string
      */
-	function load_path($path)
-	{
-		return site_url() . $path;
-	}
+    function load_path($path)
+    {
+        return site_url() . $path;
+    }
 }
 
 if ( ! function_exists('resource')) 
 {
-	/**
+    /**
      * Load files from resource folder
      * by the use of CodeIgniter's site_url() function
      * 
      * @param  string   $path
      * @return string 
      */
-	function resource($path = null, $item = null)
-	{
+    function resource($path = null, $item = null)
+    {
         $path = dot2slash($path);
 
         if ( ! is_null($path)) {
-		    $path = 'resources' . DIRECTORY_SEPARATOR . $path;
+            $path = 'resources' . DIRECTORY_SEPARATOR . $path;
         }
 
         if ( ! is_null($item)) {
             $path .= '/'.$item;
         }
         
-		return (!empty($path)) ? load_path($path) : site_url() . ASSETS;
-	}
+        return (!empty($path)) ? load_path($path) : site_url() . ASSETS;
+    }
 }
 
 if ( ! function_exists('asset')) 
 {
-	/**
+    /**
      * Load assets folder
      * by the use of CodeIgniter's site_url() function
      * 
      * @param  string   $filepath
      * @return string 
      */
-	function asset($filepath = null)
-	{
-		if($filepath !== null) 
-		{
-			return site_url() . ASSETS . $filepath;
-		} 
+    function asset($filepath = null)
+    {
+        if($filepath !== null) 
+        {
+            return site_url() . ASSETS . $filepath;
+        } 
 
-		return site_url() . ASSETS;
+        return site_url() . ASSETS;
 
-	}
+    }
 }
 
-if ( ! function_exists('use_asset')) 
-{
-    /**
-     * Load assets for a package
-     * This targets a module placed in app/Packages
-     *
-     * @param string $package
-     * @param string $filepath
-     * @return string
-     */
-	function use_asset($filepath = null, $package = '')
-	{
-        $path = 'packages';
-        $assets = 'assets';
-
-		if ($filepath === null) {
-            throw new \Exception('filepath needs to be specified');
-		}
-
-        if (empty($package)) {
-            $package = strtolower(ci('router')->fetch_module());
-        }
-
-        return load_path($path .DS. $package) .DS.$assets.DS. $filepath;
-	}
-}
 
 if ( ! function_exists('img')) 
 {
@@ -170,10 +144,10 @@ if ( ! function_exists('img'))
      * @param string $filepath
      * @return string
      */
-	function img($filepath = null)
-	{
-		return asset($filepath);
-	}
+    function img($filepath = null)
+    {
+        return asset($filepath);
+    }
 }
 
 if ( ! function_exists('css')) 
@@ -184,10 +158,10 @@ if ( ! function_exists('css'))
      * @param string $filepath
      * @return string
      */
-	function css($filepath = null)
-	{
-		return asset($filepath);
-	}
+    function css($filepath = null)
+    {
+        return asset($filepath);
+    }
 }
 
 if ( ! function_exists('js')) 
@@ -198,8 +172,88 @@ if ( ! function_exists('js'))
      * @param string $filepath
      * @return string
      */
-	function js($filepath = null)
-	{
-		return asset($filepath);
-	}
+    function js($filepath = null)
+    {
+        return asset($filepath);
+    }
+}
+
+
+if ( ! function_exists('use_url')) 
+{
+    /**
+     * Use an external asset from
+     * a given url or source
+     *
+     * @param string $filepath
+     * @param string $package
+     * @return string
+     */
+    function use_url($url = null, $ext = false)
+    {
+        return ($ext) ? str_ext($url) : $url;
+    }
+}
+
+if ( ! function_exists('use_asset')) 
+{
+    /**
+     * Load assets for a package
+     * This targets a module placed in app/Packages
+     *
+     * @param string $filepath 
+     * @param string $package
+     * @return string
+     */
+    function use_asset($filepath = null, $package = '')
+    {
+        $path = 'packages';
+        $assets = 'assets';
+
+        if ($filepath === null) {
+            throw new \Exception('filepath needs to be specified');
+        }
+
+        if (empty($package)) {
+            $package = strtolower(ci('router')->fetch_module());
+        }
+
+        return load_path($path .DS. $package) .DS.$assets.DS. $filepath;
+    }
+}
+
+if ( ! function_exists('use_css')) 
+{
+    /**
+     * Alias to the above function
+     * but loads only css files
+     *
+     * @param string $filepath
+     * @param string $package
+     * @return string
+     */
+    function use_css($filepath = null, $package = '', $ext = '.css')
+    {
+        $filepath = str_ext($filepath, true);
+
+        return use_asset($filepath.$ext, $package);
+    }
+}
+
+if ( ! function_exists('use_js')) 
+{
+    /**
+     * Alias to the above function
+     * but loads only js files
+     *
+     * @param string $filepath
+     * @param string $package
+     * @return string
+     */
+    function use_js($filepath = null, $package = '', $ext = '.js')
+    {
+        $filepath = str_ext($filepath, true);
+
+        return use_asset($filepath . $ext, $package);
+    }
 }
