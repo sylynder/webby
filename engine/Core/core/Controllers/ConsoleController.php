1f07e3f06e25bf975ba29e85b3da0de2770dca82
otengkwame <developerkwame@gmail.com>
2022-06-04 11:14:32 +0000

    :hammer: improve Console Controller
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- engine/Core/core/Controllers/ConsoleController.php
+++ engine/Core/core/Controllers/ConsoleController.php
@@ -2,6 +2,8 @@
 
 namespace Base\Controllers;
 
+use Base\Console\ConsoleColor;
+
 class ConsoleController extends Controller
 {
     
@@ -25,6 +27,53 @@ class ConsoleController extends Controller
         if (ENVIRONMENT !== $this->env) {
             exit;
         }
+    }
+
+    protected function success($text, $times = 1, $nextline = true)
+    {
+        if ($nextline) {
+            return ConsoleColor::green($text) . $this->nextline($times);
+        }
+
+        return ConsoleColor::green($text);
+    }
+
+    protected function info($text, $times = 1, $nextline = true)
+    {
+        if ($nextline) {
+            return ConsoleColor::cyan($text) . $this->nextline($times);
+        }
+
+        return ConsoleColor::cyan($text);
+    }
+
+    protected function warning($text, $times = 1, $nextline = true)
+    {
+        if ($nextline) {
+            return ConsoleColor::yellow($text) . $this->nextline($times);
+        }
+
+        return ConsoleColor::yellow($text);
+    }
+
+    protected function error($text, $times = 1, $nextline = true)
+    {
+        if ($nextline) {
+            return ConsoleColor::red($text) . $this->nextline($times);
+        }
+
+        return ConsoleColor::red($text);
+    }
+
+    protected function nextline($times = 1)
+    {
+        $line = " \n";
+
+        if ($times > 1) {
+            return str_repeat($line, $times);
+        }
+
+        return $line;
     }
 
 }
