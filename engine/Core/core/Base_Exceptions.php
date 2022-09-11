c7c55490a196b52319a879e342b84fbf8e2b81eb
otengkwame <developerkwame@gmail.com>
2022-09-11 08:36:36 +0000

    :sparkles: identify all php error levels
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- engine/Core/core/Base_Exceptions.php
+++ engine/Core/core/Base_Exceptions.php
@@ -4,6 +4,27 @@ defined('COREPATH') or exit('No direct script access allowed');
 class Base_Exceptions extends \CI_Exceptions
 {
 
+	/**
+	 * List of available error levels
+	 *
+	 * @var	array
+	 */
+	public $levels = [
+		E_ERROR			=>	'Error',
+		E_WARNING		=>	'Warning',
+		E_PARSE			=>	'Parsing Error',
+		E_NOTICE		=>	'Notice',
+		E_CORE_ERROR		=>	'Core Error',
+		E_CORE_WARNING		=>	'Core Warning',
+		E_COMPILE_ERROR		=>	'Compile Error',
+		E_COMPILE_WARNING	=>	'Compile Warning',
+		E_USER_ERROR		=>	'User Error',
+		E_USER_WARNING		=>	'User Warning',
+		E_USER_NOTICE		=>	'User Notice',
+		E_STRICT		=>	'Runtime Notice',
+		E_DEPRECATED => "Deprecated"
+	];
+
 	public function __construct()
 	{
 		parent::__construct();
