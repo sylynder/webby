aea391860d73b924b847f77491728130210d3486
otengkwame <developerkwame@gmail.com>
2022-09-22 15:52:25 +0000

    :sparkles: add add_foreign_key() helper function
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- engine/Core/helpers/db_helper.php
+++ engine/Core/helpers/db_helper.php
@@ -93,6 +93,26 @@ if ( ! function_exists( 'max_id' ))
     }
 }
 
+
+if ( ! function_exists('add_foreign_key'))
+{
+	/**
+	 * @param string $table       Table name
+	 * @param string $foreign_key Collumn name having the Foreign Key
+	 * @param string $references  Table and column reference. Ex: users(id)
+	 * @param string $on_delete   RESTRICT, NO ACTION, CASCADE, SET NULL, SET DEFAULT
+	 * @param string $on_update   RESTRICT, NO ACTION, CASCADE, SET NULL, SET DEFAULT
+	 *
+	 * @return string SQL command
+	 */
+	function add_foreign_key($table, $foreign_key, $references, $on_delete = 'RESTRICT', $on_update = 'RESTRICT')
+	{
+		$references = explode('(', str_replace(')', '', str_replace('`', '', $references)));
+
+		return "ALTER TABLE `{$table}` ADD CONSTRAINT `{$table}_{$foreign_key}_fk` FOREIGN KEY (`{$foreign_key}`) REFERENCES `{$references[0]}`(`{$references[1]}`) ON DELETE {$on_delete} ON UPDATE {$on_update}";
+	}
+}
+
 if ( ! function_exists('drop_foreign_key'))
 {
 	/**
